867fea412a86dc459ccb835c7a06281c5aab86e5
kierkirby32 <kierkirby32@gmail.com>
2024-05-26 01:12:01 +0800

    kaa-portfolio-upload

---
+++ wp-content/plugins/dracula-dark-mode/blocks/build/switch/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components'), 'version' => 'a6cea37b708a753d2853');
---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1716657044/wp-content/uploads/uag-plugin/assets/2000/uag-js-1608.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-0dbf6bbe' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-103f0f59' );
+});
+ });
